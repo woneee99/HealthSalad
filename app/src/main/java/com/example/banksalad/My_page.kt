c2d0aa9cb54dc14c88c26a3571fff1f90019840f
wonee <hwang9821@icloud.com>
2021-11-09 17:50:43 +0900

    11/9 update

--- app/src/main/java/com/example/banksalad/My_page.kt
+++
@@ -1,4 +0,0 @@
-package com.example.banksalad
-
-class My_page {
-}
---
+++ app/src/main/java/com/example/banksalad/chartAct.java
@@ -0,0 +1,63 @@
+package com.example.banksalad;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.constraintlayout.motion.utils.Easing;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class chartAct extends Activity {
+    private LineChart lineChart;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chart);
+
+        lineChart = (LineChart)findViewById(R.id.linechart);
+        List<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(11, 53));
+        entries.add(new Entry(12, 52));
+        entries.add(new Entry(13, 50));
+        entries.add(new Entry(14, 54));
+        entries.add(new Entry(15, 53));
+        LineDataSet lineDataSet = new LineDataSet(entries, "속성명1");
+        lineDataSet.setLineWidth(2);
+        lineDataSet.setCircleRadius(6);
+        lineDataSet.setCircleColor(Color.parseColor("#FFA1B4DC"));
+        lineDataSet.setColor(Color.parseColor("#FFA1B4DC"));
+        lineDataSet.setDrawCircleHole(true);
+        lineDataSet.setDrawCircles(true);
+        lineDataSet.setDrawHorizontalHighlightIndicator(false);
+        lineDataSet.setDrawHighlightIndicators(false);
+        lineDataSet.setDrawValues(false);
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.enableGridDashedLine(8, 24, 0);
+        YAxis yLAxis = lineChart.getAxisLeft();
+        yLAxis.setTextColor(Color.BLACK);
+        YAxis yRAxis = lineChart.getAxisRight();
+        yRAxis.setDrawLabels(false);
+        yRAxis.setDrawAxisLine(false);
+        yRAxis.setDrawGridLines(false);
+        Description description = new Description();
+        description.setText("");
+        lineChart.setDoubleTapToZoomEnabled(false);
+        lineChart.setDrawGridBackground(false);
+        lineChart.setDescription(description);
+        lineChart.invalidate();
+    }
+}
